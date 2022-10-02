9ac0bee014d35b03dfb1d139cd84313010ab01fa
Rishmika Sandanu <106467954+ImRishmika@users.noreply.github.com>
2022-10-02 14:15:42 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -152,7 +152,7 @@ async def not_joined(client: Client, message: Message):
 async def get_users(client: Bot, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
     users = await full_userbase()
-    await msg.edit(f"{len(users)} users are using this bot")
+    await msg.edit(f"☘ Total Users :-{len(users)} \n\n 💾 Disk Space :- 48.25% \n\n 💽 Database Type :- Telegram Seved Massege  \n\n 👨‍💻 Devoloper :- @ImRishmika")
 
 @Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
