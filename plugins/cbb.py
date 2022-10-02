1b40625ac1020850de40931b633f07fcaa7e9fc1
Rishmika Sandanu <106467954+ImRishmika@users.noreply.github.com>
2022-10-02 09:18:18 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>👨‍💻 Creator : <a href='tg://user?id={OWNER_ID}'>Rishmika Sandanu</a>\n 🎈 Language : <code>Python3</code>\n📚 Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n ☘ Channel : @EmoBotDevolopers\n ☘ Support Group : @EmoBotSupport</b>",
+            text = f"<b>👨‍💻 Creator : <a href='tg://user?id={OWNER_ID}'>Rishmika Sandanu</a>\n\n 🎈 Language : <code>Python3</code>\n\n📚 Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n\n ☘ Channel : @EmoBotDevolopers\n\n ☘ Support Group : @EmoBotSupport</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
